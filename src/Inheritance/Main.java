60d9bf71c1815bc1b6609c0239da195e5fb20a12
Artyom Pionov <pionovartem11@gmail.com>
2023-03-05 16:52:51 +0000

    First Commit

---
+++ src/Inheritance/Main.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        // From Animal and Cat classes learning
+//        Cat cat = new Cat();
+//        cat.sound();
+//        System.out.println(cat.getEats());
+
+        ColorPoint point = new ColorPoint(3.5, 5.5, 1);
+        point.print();
+    }
+}
