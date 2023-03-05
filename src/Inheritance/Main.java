97645776ba6da76b37bffb1c9db2377fd4d88ee7
Artyom Pionov <pionovartem11@gmail.com>
2023-03-05 18:01:57 +0000

    3rd Commit

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -1,13 +1,18 @@
 package Inheritance;
 
+@SuppressWarnings("CommentedOutCode") // To make the code nice :)
+
 public class Main {
     public static void main(String[] args) {
-        // From Animal and Cat classes learning
+        //---From Animal and Cat classes learning---
+
 //        Cat cat = new Cat();
 //        cat.sound();
 //        System.out.println(cat.getEats());
 
-        ColorPoint point = new ColorPoint(3.5, 5.5, 1);
-        point.print();
+        //---From Point and ColorPoint class---
+
+//        ColorPoint point = new ColorPoint(3.5, 5.5, 1);
+//        point.print();
     }
 }
